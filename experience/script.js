079da690f6b2ab7b13f8acc58731722f3acb9e58
sam-pillutla <137445299+sam-pillutla@users.noreply.github.com>
2024-06-21 15:01:02 +0530

    Update script.js

--- experience/script.js
+++ experience/script.js
@@ -68,7 +68,7 @@ document.onkeydown = function(e) {
 document.addEventListener('visibilitychange',
 function(){
     if(document.visibilityState === "visible"){
-        document.title = "Experience | Portfolio Guna Nalam";
+        document.title = "Experience | Portfolio Sam Pillutla";
         $("#favicon").attr("href","/assets/images/favicon.png");
     }
     // else {
